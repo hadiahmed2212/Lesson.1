a2a4d066f427774f5e70ccde9444c8875ee749d4
hadiahmed2212 <hadiahmed2212@gmail.com>
2026-06-15 20:42:38 +0800

    profit loss

---
+++ profit loss.py
@@ -0,0 +1,4 @@
+baughtfor=float(input("pls enter the amount u baught it for :)."))
+sellfor=float(input("pls enter the amount u sold it for :)."))
+if sellfor >baughtfor: print ("you have gained profit")
+else: print ("you have became broke")
