7f10ca728304fd78256b4d22843343ffbfa2231c
hadiahmed2212 <hadiahmed2212@gmail.com>
2026-06-29 20:48:34 +0800

    not=to

---
+++ not = to.py
@@ -0,0 +1,10 @@
+a,b,c = 67,40,67
+if a != b:
+    print("it is not =")
+else: print("it is =")
+if a !=c:
+    print("it is not =")
+else: print("it is =")
+if not (a==1) == (b==5):
+    print("the condition is True.")
+else: print("the condition is false.")
