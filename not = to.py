f3f78dafcd83050aeba1af83eaf67e58c011728c
hadiahmed2212 <hadiahmed2212@gmail.com>
2026-07-01 20:26:44 +0800

    indent operators.

--- not = to.py
+++ not = to.py
@@ -1,4 +1,6 @@
-a,b,c = 67,40,67
+a =int(input("enter an amount and we will calculate if the condion is true or false"))
+b=int(input("enter another amount and we will calculate"))
+c=int(input("enter one more amount and we will calculate"))
 if a != b:
     print("it is not =")
 else: print("it is =")
