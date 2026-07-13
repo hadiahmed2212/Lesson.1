08e61a401b8e62df876e95b03fe5b922e6e49fdc
hadiahmed2212 <hadiahmed2212@gmail.com>
2026-07-13 18:24:01 +0800

    after class

---
+++ after class project using nested if statements.py
@@ -0,0 +1,16 @@
+holiday_type= int(input("1 = bech type holiday 2 = mountain type holiday"))
+if holiday_type == 1:
+    beach_type = int(input("you chose beach holiday now choose if you want to pick swimming (1) or making a sandcastle(2)"))
+    if beach_type == 1:
+        print("you picked the resort beach holiday!")
+    else:
+        print("you picked the camping beach holiday!")
+elif holiday_type == 2:
+    mountain_type = int(input("you chose mountain holiday now choose if you want to pick camping (1) or hiking(2)"))
+    if mountain_type == 1:
+        print("your final decision was camping, enjoy your new camping trip!")
+    else:
+        print ("your final decison was hiking, enjoy your new hiking trip!")
+if holiday_type != 1 and holiday_type != 2:
+    print("ERROR WRONG INPUT!")
+# vs code helped me using AI to press tab to indent the code and make it more readable. I also used AI to help me with the nested if statements.
